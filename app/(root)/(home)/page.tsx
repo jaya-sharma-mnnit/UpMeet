1bf4141a034a7e525a2a298e199479040733311f
jaya-sharma-mnnit <sjaya211004@gmail.com>
2025-04-12 17:37:09 +0530

    undo home route protect

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,22 +1,8 @@
 import MeetingTypeList from '@/components/MeetingTypeList';
 import React from 'react';
-import { redirect } from 'next/navigation';
-import { cookies } from 'next/headers';
-import  jwt  from 'jsonwebtoken';
 
 const Home = async() => {
-  const cookieStore = await cookies();
-  const token = cookieStore.get('token'); // or whatever key you use
-
-  if (!token) {
-    redirect('/sign-in');
-  }
-  try {
-    const user = jwt.verify(token.value, process.env.JWT_SECRET!);
-    // Render home page using `user` data
-  } catch {
-    redirect('/sign-in');
-  }
+  
   const now =new Date();
   const year = now.getFullYear();
   const month = now.toLocaleDateString('en-US', { month: 'long' });
